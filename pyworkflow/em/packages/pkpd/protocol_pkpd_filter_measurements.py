4f8d4a3eb9f3db0d8c9b720ca16d78a217e94207
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2016-04-16 09:49:01 +0200

    Remove NA implemented

--- pyworkflow/em/packages/pkpd/protocol_pkpd_filter_measurements.py
+++ pyworkflow/em/packages/pkpd/protocol_pkpd_filter_measurements.py
@@ -24,11 +24,9 @@
 # *
 # **************************************************************************
 
-import sys
-
 import pyworkflow.protocol.params as params
 from pyworkflow.em.protocol.protocol_pkpd import ProtPKPD
-from pyworkflow.em.data import PKPDExperiment
+from pyworkflow.em.data import PKPDExperiment, PKPDSample
 
 
 class ProtPKPDFilterMeasurements(ProtPKPD):
@@ -45,7 +43,7 @@ class ProtPKPDFilterMeasurements(ProtPKPD):
         form.addParam('filterType', params.EnumParam, choices=["Exclude","Keep","Remove NA"], label="Filter mode", default=0,
                       help='Exclude or keep measurements meeting the following condition.\n"\
                            "NA values are excluded in keep filters, and kept in exclude filters')
-        form.addParam('condition', params.TextParam, label="Condition",
+        form.addParam('condition', params.TextParam, label="Condition", condition="filterType!=2",
                       help='Example: $(t)<200\n $(Cp)>=1000 and $(Cp)<=2000"')
 
     #--------------------------- INSERT steps functions --------------------------------------------
@@ -57,15 +55,72 @@ class ProtPKPDFilterMeasurements(ProtPKPD):
 
     #--------------------------- STEPS functions --------------------------------------------
     def runFilter(self, objId, filterType, condition):
+        import copy
         experiment = self.readExperiment(self.inputExperiment.get().fnPKPD)
+
         self.printSection("Filtering")
         if self.filterType.get()==0:
             filterType="exclude"
-        else:
+        elif self.filterType.get()==1:
             filterType="keep"
-        self.experiment = experiment.filterSamples(self.condition.get(),filterType)
-        self.experiment._printToStream(sys.stdout)
-        self.experiment.write(self._getPath("experiment.pkpd"))
+        else:
+            filterType="rmNA"
+
+        filteredExperiment = PKPDExperiment()
+        filteredExperiment.general = copy.copy(experiment.general)
+        filteredExperiment.variables = copy.copy(experiment.variables)
+        filteredExperiment.samples = {}
+        filteredExperiment.doses = {}
+
+        usedDoses = []
+        for sampleKey, sample in experiment.samples.iteritems():
+            candidateSample = PKPDSample()
+            candidateSample.variableDictPtr    = copy.copy(sample.variableDictPtr)
+            candidateSample.doseDictPtr        = copy.copy(sample.doseDictPtr)
+            candidateSample.varName            = copy.copy(sample.varName)
+            candidateSample.doseName           = copy.copy(sample.doseName)
+            candidateSample.dose               = copy.copy(sample.dose)
+            candidateSample.descriptors        = copy.copy(sample.descriptors)
+            candidateSample.measurementPattern = copy.copy(sample.measurementPattern)
+
+            N = 0 # Number of initial measurements
+            if len(sample.measurementPattern)>0:
+                aux=getattr(sample,"measurement_%s"%sample.measurementPattern[0])
+                N = len(aux)
+            if N==0:
+                continue
+
+            # Create empty output variables
+            Nvar = len(sample.measurementPattern)
+            for i in range(0,Nvar):
+                exec("candidateSample.measurement_%s = []"%sample.measurementPattern[i])
+
+            for n in range(0,N):
+                toAdd = []
+                okToAddTimePoint = True
+                for i in range(0,Nvar):
+                    exec("aux=sample.measurement_%s[%d]"%(sample.measurementPattern[i],n))
+                    # aux=getattr(sample,"measurement_%s"%sample.measurementPattern[i])
+                    if filterType=="rmNA":
+                        if aux=="NA":
+                            okToAddTimePoint = False
+                        else:
+                            toAdd.append(aux)
+                if okToAddTimePoint:
+                    for i in range(0,Nvar):
+                        exec("candidateSample.measurement_%s.append('%s')"%(sample.measurementPattern[i],toAdd[i]))
+
+            N = len(getattr(sample,"measurement_%s"%sample.measurementPattern[0])) # Number of final measurements
+            if N!=0:
+                filteredExperiment.samples[candidateSample.varName] = candidateSample
+                usedDoses.append(candidateSample.doseName)
+
+        if len(usedDoses)>0:
+            for doseName in usedDoses:
+                filteredExperiment.doses[doseName] = copy.copy(experiment.doses[doseName])
+
+        self.writeExperiment(filteredExperiment,self._getPath("experiment.pkpd"))
+        self.experiment = filteredExperiment
 
     def createOutputStep(self):
         self._defineOutputs(outputExperiment=self.experiment)
