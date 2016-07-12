10dfd51d30035b9ff6b304d58b50b23a46e9e859
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2016-07-12 22:08:07 +0200

    Merge labels added

--- pyworkflow/em/packages/pkpd/__init__.py
+++ pyworkflow/em/packages/pkpd/__init__.py
@@ -40,6 +40,8 @@ from protocol_pkpd_import_from_csv import ProtPKPDImportFromCSV
 from protocol_pkpd_simulate_generic_pd import ProtPKPDSimulateGenericPD
 from protocol_pkpd_pdgeneric_fit import ProtPKPDGenericFit
 from protocol_pkpd_create_label import ProtPKPDCreateLabel
+from protocol_pkpd_merge_labels import ProtPKPDMergeLabels
+
 
 from protocol_batch_create_experiment import BatchProtCreateExperiment
 
