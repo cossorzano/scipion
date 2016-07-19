fab9c61c52d4c44c1f23a64de00186867d9160fb
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2016-07-19 08:15:17 +0200

    F and MAT estimated

--- pyworkflow/em/packages/pkpd/protocol_pkpd_nca_niv.py
+++ pyworkflow/em/packages/pkpd/protocol_pkpd_nca_niv.py
@@ -27,7 +27,7 @@
 import pyworkflow.protocol.params as params
 from protocol_pkpd_sa_base import ProtPKPDSABase
 from sa_models import NCANIVModel
-from pyworkflow.em.pkpd_units import PKPDUnit
+from pyworkflow.em.pkpd_units import PKPDUnit, strUnit
 from pyworkflow.protocol.constants import LEVEL_ADVANCED
 
 class ProtPKPDNCANIV(ProtPKPDSABase):
@@ -38,13 +38,17 @@ class ProtPKPDNCANIV(ProtPKPDSABase):
     #--------------------------- DEFINE param functions --------------------------------------------
     def _defineParams(self, form):
         form.addSection('Input')
-        form.addParam('protAbsorption', params.PointerParam, label="Input experiment",
-                      pointerClass='ProtPKPDAbsorptionRate',
+        form.addParam('protAbsorption', params.PointerParam, label="Non-IV Input experiment",
+                      pointerClass='ProtPKPDAbsorptionRate', important=True,
                       help='Select an execution of a protocol estimating the absorption rate.')
         form.addParam('areaCalc', params.EnumParam, choices=["Trapezoidal","Mixed"],
                       label="Method for AUC, AUMC calculation", default=1, expertLevel=LEVEL_ADVANCED,
                       help='The mixed integration uses the trapezoidal is used in the raising side and the log-trapezoidal in the decay side.\n'
                             'See explanation at http://learnpkpd.com/2011/04/02/calculating-auc-linear-and-log-linear\n')
+        form.addParam('protNCAIV', params.PointerParam, label="IV Input experiment", allowsNull=True,
+                      pointerClass='ProtPKPDNCAIVExp,ProtPKPDNCAIVObs',
+                      help='A companion experiment with intraveneous bolus is used to estimate the bioavailability and the Mean Absorption Time. '
+                           'The sample names in both experiments must be exactly the same.')
 
     def getListOfFormDependencies(self):
         return [self.protAbsorption.get().getObjId()]
@@ -54,7 +58,9 @@ class ProtPKPDNCANIV(ProtPKPDSABase):
         return self.protAbsorption.get().outputExperiment
 
     def setupFromFormParameters(self):
-        pass
+        self.experimentIV = None
+        if self.protNCAIV.get() != None:
+            self.experimentIV=self.readExperiment(self.protNCAIV.get().outputExperiment.fnPKPD)
 
     def getXYvars(self):
         self.varNameX = self.protAbsorption.get().protElimination.get().predictor.get()
@@ -77,6 +83,23 @@ class ProtPKPDNCANIV(ProtPKPDSABase):
         self.analysis.D = sample.getDoseAt(0.0)
         self.analysis.Ke = float(sample.descriptors["Ke"])
         return True
+
+    def postAnalysis(self, sampleName):
+        if self.experimentIV!=None:
+            if sampleName in self.experimentIV.samples:
+                sampleNIV = self.experiment.samples[sampleName]
+                DNIV= self.analysis.D
+
+                sampleIV = self.experimentIV.samples[sampleName]
+                sampleIV.interpretDose()
+                DIV = sampleIV.getDoseAt(0.0)
+
+                F = float(sampleNIV.descriptors['AUC_0inf'])/float(sampleNIV.descriptors['AUC_0inf'])*DIV/DNIV
+                MAT = float(sampleNIV.descriptors['MRT']) - float(sampleIV.descriptors['MRT'])
+                self.experiment.addParameterToSample(sampleName, "F", PKPDUnit.UNIT_NONE, "Estimated bioavailability", F)
+                self.experiment.addParameterToSample(sampleName, "MAT", self.analysis.parameterUnits[-1], "Estimated Mean Absorption Time", MAT)
+                print("F = %f"%F)
+                print("MAT = %f [%s]"%(MAT,strUnit(self.analysis.parameterUnits[-1])))
 
     #--------------------------- INFO functions --------------------------------------------
     def _summary(self):
