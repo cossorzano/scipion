7d86b356d54692a267c9cef78d2add0865813a47
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2016-08-06 01:48:34 +0200

    Filter population added

--- pyworkflow/em/packages/pkpd/__init__.py
+++ pyworkflow/em/packages/pkpd/__init__.py
@@ -54,6 +54,7 @@ from protocol_pkpd_change_units import ProtPKPDChangeUnits
 from protocol_pkpd_ev_monocompartment import ProtPKPDEV1MonoCompartment
 from protocol_pkpd_ode_bootstrap import ProtPKPDODEBootstrap
 from protocol_pkpd_merge_populations import ProtPKPDMergePopulations
+from protocol_pkpd_filter_population import ProtPKPDFilterPopulation
 
 
 from protocol_batch_create_experiment import BatchProtCreateExperiment
