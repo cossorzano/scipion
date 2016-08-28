51cbf81d557d688a5553a298df0725b36ab3c871
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2016-08-28 09:06:47 +0200

    Zero order absorption

--- pyworkflow/em/packages/pkpd/protocol_pkpd_ev0_monocompartment.py
+++ pyworkflow/em/packages/pkpd/protocol_pkpd_ev0_monocompartment.py
@@ -31,67 +31,30 @@ import biopharmaceutics
 
 
 class ProtPKPDEV0MonoCompartment(ProtPKPDODEBase):
-    """ Fit a monocompartmental model with first order absorption to a set of measurements obtained by oral doses (any arbitrary dosing regimen is allowed)\n
-        The differential equation is D(t)=Amax*(1-exp(-Ka*t)) and dC/dt = -Cl * C + 1/V * dD/dt\n
-        where C is the concentration, Cl the clearance, V the distribution volume, and D the input dosing regime. Ka is the absorbption rate and
+    """ Fit a monocompartmental model with zero order absorption to a set of measurements obtained by oral doses (any arbitrary dosing regimen is allowed)\n
+        The differential equation is D(t)=Rin*t and dC/dt = -Cl * C + 1/V * dD/dt\n
+        where C is the concentration, Cl the clearance, V the distribution volume, and D the input dosing regime. Rin is the absorbption rate and
         Amax the dose (maximum amount).
 Confidence intervals calculated by this fitting may be pessimistic because it assumes that all model parameters
 are independent, which are not. Use Bootstrap estimates instead.\n
         Protocol created by http://www.kinestatpharma.com\n"""
     _label = 'ev0 monocompartment'
 
+    def __init__(self,**kwargs):
+        ProtPKPDODEBase.__init__(self,**kwargs)
+        self.compulsoryBounds = True
+
     #--------------------------- DEFINE param functions --------------------------------------------
     def _defineParams(self, form):
         self._defineParams1(form, True, "t", "Cp")
-        form.addParam('initType', params.EnumParam, choices=["From absorption protocol","Specify bounds"], label="Initialize from", default=0,
-                      help='You may initialize the model from estimates of a previous protocol or by giving the bounds for the different parameters')
-        form.addParam('absorptionProtocol', params.PointerParam, label="Absorption rate protocol",
-                      pointerClass='ProtPKPDAbsorptionRate', allowsNull = True, condition="initType==0",
-                      help='Absorption rate Protocol')
-        form.addParam('bounds', params.StringParam, label="Parameter bounds ([tlag], Ka, Cl, V)", default="", condition="initType==1",
+        form.addParam('bounds', params.StringParam, label="Parameter bounds ([tlag], Rin, Cl, V)", default="",
                       help="Bounds for the tlag (if it must be estimated), absorption, clearance and volume. Example: (0.01,0.04);(0.2,0.4);(10,20). "\
                       'Make sure that the bounds are expressed in the expected units (estimated from the sample itself).'\
                       'Be careful that Cl bounds must be given here. If you have an estimate of the elimination rate, this is Ke=Cl/V. Consequently, Cl=Ke*V ')
 
-    def setupFromFormParameters(self):
-        if self.initType==0:
-            self.absProt = self.absorptionProtocol.get()
-            self.absExperiment = self.readExperiment(self.absProt.outputExperiment.fnPKPD)
-        else:
-            self.ncaProt = None
-
-    def setBounds(self,sample):
-        if self.initType == 0:
-            boundsString = ""
-            if sample.getSampleName() in self.absExperiment.samples:
-                sampleAbs = self.absExperiment.samples[sample.getSampleName()]
-                Ka = float(sampleAbs.descriptors["Ka"])
-                Ke = float(sampleAbs.descriptors["Ke"])
-                V = float(sampleAbs.descriptors["Vd"])
-                Cl = Ke*V
-                boundsString = "(%f,%f);(%f,%f);(%f,%f)"%(Ka/3,Ka*3,Cl/3,Cl*3,V/3,V*3)
-                if self.findtlag:
-                    tlag = float(sampleAbs.descriptors["tlag"])
-                    boundsString = "(%f,%f);%s"%(tlag/3,tlag*3,boundsString)
-            self.parseBounds(boundsString)
-            self.setBoundsFromBoundsList()
-        else:
-            ProtPKPDODEBase.setBounds(self,sample)
-
     def configureSource(self, drugSource):
         drugSource.type = biopharmaceutics.DrugSource.EV
-        drugSource.evProfile = biopharmaceutics.BiopharmaceuticsModelOrder1()
+        drugSource.evProfile = biopharmaceutics.BiopharmaceuticsModelOrder0()
 
     def createModel(self):
         return PK_Monocompartment()
-
-    def _validate(self):
-        self.getXYvars()
-        errors=[]
-        if self.varNameX!=None:
-            experiment = self.readExperiment(self.getInputExperiment().fnPKPD, False)
-            if not self.varNameX in experiment.variables:
-                errors.append("Cannot find %s as variable"%self.varNameX)
-            if not self.varNameY in experiment.variables:
-                errors.append("Cannot find %s as variable"%self.varNameY)
-        return errors
