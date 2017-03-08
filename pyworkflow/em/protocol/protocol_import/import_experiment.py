74fadad00f237a04f96bbab02cc5c6f645c868e8
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2017-03-08 23:28:27 +0100

    Comment where the protocol is tested added

--- pyworkflow/em/protocol/protocol_import/import_experiment.py
+++ pyworkflow/em/protocol/protocol_import/import_experiment.py
@@ -54,6 +54,7 @@ from pyworkflow.utils.path import copyFile
 # TESTED in test_workflow_gabrielsson_pk39.py
 # TESTED in test_workflow_gabrielsson_pk43.py
 # TESTED in test_workflow_gabrielsson_pd03.py
+# TESTED in test_workflow_gabrielsson_pd11.py
 
 class ProtImportExperiment(ProtImportFiles):
     """ Protocol to import an PKPD experiment\n
